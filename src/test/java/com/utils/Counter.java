065d24ea9ce93ee02d75f3d45c9c7498b190a1b8
lgb0538@126.com <Dave>
2018-06-07 21:34:23 +0800

    清理垃圾文件

--- src/test/java/com/utils/Counter.java
+++ src/test/java/com/utils/Counter.java
@@ -7,35 +7,35 @@ public class Counter {
 	public static int i = 0;
 	public static int showSteps = 0;
 	public static int steps = 1;
-	public static List<Integer> caches = new ArrayList<>();
-
+	public static List<Integer> caches = new ArrayList<Integer>();
+	
 	public static void add() {
 		i += steps;
 		if (showSteps > 0 && i % showSteps == 0) {
 			System.out.println(i);
 		}
 	}
-
+	
 	public static void add(int psteps) {
 		i += psteps;
 		if (showSteps > 0 && i % showSteps == 0) {
 			System.out.println(i);
 		}
 	}
-
+	
 	public static void caches() {
 		caches.add(new Integer(i));
 	}
-
+	
 	public static void init() {
 		i = 0;
 		showSteps = 0;
 	}
-
+	
 	public static void print() {
 		System.out.println("当前计数：" + i);
 	}
-
+	
 	public static void printCaches() {
 		int j = 0;
 		for (int ic : caches) {
