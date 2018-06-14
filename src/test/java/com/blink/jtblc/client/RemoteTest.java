1080b4d33f6558d5dc5f8018aa6c5f107b360981
Administrator <Administrator@SuperZzz1991>
2018-06-14 19:43:21 +0800

    init

--- src/test/java/com/blink/jtblc/client/RemoteTest.java
+++ src/test/java/com/blink/jtblc/client/RemoteTest.java
@@ -37,21 +37,21 @@ public class RemoteTest {
 	Remote remote = new Remote(conn);
 	
 	// 4.4 请求底层服务器信息
-	//@Test
+	@Test
 	public void requestServerInfoTest() {
 		ServerInfo bean = remote.requestServerInfo();
 		System.out.println("state:" + bean.getServerState());
 	}
 	
 	// 4.5 获取最新账本信息
-	//@Test
+	@Test
 	public void requestLedgerClosedTest() throws Exception {
 		LedgerClosed bean = remote.requestLedgerClosed();
 		System.out.println("ledger_hash:" + bean.getLedgerHash());
 	}
 	
 	// 4.6 获取某一账本具体信息
-	//@Test
+	@Test
 	public void requestLedgerTest() {
 		String ledger_index = "431549";
 		String ledger_hash = "";
@@ -61,7 +61,7 @@ public class RemoteTest {
 	}
 	
 	// 4.7 查询某一交易具体信息
-	//@Test
+	@Test
 	public void requestTxTest() {
 		String hash = "EEE011C81317A422895D0570CE792B3547BF848EDA6C6E1751162F21FB7D2098";
 		Account bean = remote.requestTx(hash);
@@ -69,21 +69,21 @@ public class RemoteTest {
 	}
 	
 	// 4.8 请求账号信息
-	//@Test
+	@Test
 	public void requestAccountInfoTest() {
 		AccountInfo bean = remote.requestAccountInfo(account_2);
 		System.out.println("账号金额：" + bean.getAccountData().getBalance());
 	}
 
 	// 4.9 获得账号可接收和发送的货币
-	//@Test
+	@Test
 	public void requestAccountTumsTest() {
 		AccountTums bean = remote.requestAccountTums(account_2);
 		System.out.println("requestAccountTumsTest:" + bean.getLedgerIndex());
 	}
 	
 	// 4.10 获得账号关系
-	//@Test
+	@Test
 	public void requestAccountRelationsTest() {
 		String type = "trust";
 		AccountRelations bean = remote.requestAccountRelations(account_1, type);
@@ -98,14 +98,14 @@ public class RemoteTest {
 	}
 	
 	// 4.12 获得账号交易列表
-	//@Test
+	@Test
 	public void requestAccountTxTest() {
 		AccountTx bean = remote.requestAccountTx(account_2, limit_num);
 		System.out.println("requestAccountTxTest:" + bean.getAccount());
 	}
 	
 	// 4.13 获得市场挂单列表
-	//@Test
+	@Test
 	public void requestOrderBookTest() {
 		String getsCurrency = "SWT";
 		String getsIssuer = "";
@@ -122,7 +122,7 @@ public class RemoteTest {
 	}
 	
 	// 4.14 支付
-	//@Test
+	@Test
 	public void buildPaymentTxTest() {
 		// 使用签名
 		// Remote remote = new Remote(conn,true);
@@ -145,7 +145,7 @@ public class RemoteTest {
 	}
 	
 	// 4.15 设置关系
-	//@Test
+	@Test
 	public void buildRelationTxTest() {
 		String type = "trust";
 		String value = "0.00005";
@@ -161,7 +161,7 @@ public class RemoteTest {
 	}
 	
 	// 4.16 设置账号属性
-	//@Test
+	@Test
 	public void buildAccountSetTxTest() {
 		String type = "property";
 		String set_flag = "";
@@ -172,7 +172,7 @@ public class RemoteTest {
 	}
 	
 	// 4.17 挂单
-	//@Test
+	@Test
 	public void buildOfferCreateTxTest() {
 		String type = "Sell";
 		Amount getsAmount = new Amount();
@@ -190,7 +190,7 @@ public class RemoteTest {
 	}
 	
 	// 4.18 取消挂单
-	//@Test
+	@Test
 	public void buildOfferCancelTxTest() {
 		Integer sequence = 22;
 		// 可选
